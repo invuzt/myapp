af49e5ce3b817990770f795b39090df162e0199a
invuzt <invuzt@gmail.com>
2026-03-26 23:46:10 +0700

    feat: input nama + button + toast

--- app/src/main/java/com/myapp/MainActivity.java
+++ app/src/main/java/com/myapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText inputNama = findViewById(R.id.inputNama);
+        Button btnSapa = findViewById(R.id.btnSapa);
+
+        btnSapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = inputNama.getText().toString().trim();
+                if (nama.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Nama tidak boleh kosong!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Halo, " + nama + "!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
